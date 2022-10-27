4ed34e69f8a4daa3226851cc7ced6801702ebe1b
Olanrewaju Alawode <olalawode@unionbankng.com>
2022-10-27 13:32:59 +0100

    third commit

--- testsite/main.py
+++ testsite/main.py
@@ -1,2 +1,9 @@
 def app(event):
-    return { "slackUsername": 'Olanrewaju', "backend": True, "age": 911, "bio": 'Hi! I am Lanre!' }
+    val = {
+        'slackUsername': 'Olanrewaju',
+        'backend': True,
+        'age': 911, 
+        'bio': 'Hi! I am Lanre!' 
+    }
+
+    return val
